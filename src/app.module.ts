b9daffb1ae5f2d9ef40ab9e4a6ea24dcaec7fc8e
Abraham Noris <abraham.noris@definityfirst.com>
2023-08-09 00:25:48 -0700

    CRUD endpoints generated by nest generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma/prisma.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/events/events.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+
+@Injectable()
+export class EventsService {
+  constructor(private prisma: PrismaService) { }
+
+  create(createEventDto: CreateEventDto) {
+    return 'This action adds a new event';
+  }
+
+  findAll() {
+    return this.prisma.event.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.event.findUnique({
+      where: {
+        event_id: id
+      }
+    })
+  }
+
+  update(id: number, updateEventDto: UpdateEventDto) {
+    return `This action updates a #${id} event`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} event`;
+  }
+}
