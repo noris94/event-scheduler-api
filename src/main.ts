19bd5b709efb4c188ebd691bdb8e778b5c6b3d15
Abraham Noris <abraham.noris@definityfirst.com>
2023-08-09 18:02:40 -0700

    fl0 port config changed

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,8 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  await app.listen(3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
